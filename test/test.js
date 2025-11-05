9d94dcab569ed6bcddac2ee825a7f0580dfabc04
Soksan Phork <Soksan.phork@student.cadt.edu.kh>
2025-11-05 11:04:06 +0700

    Update greeting message

--- test/test.js
+++ test/test.js
@@ -1,11 +1,12 @@
 import request from 'supertest';
 import { expect } from 'chai';
 import app from '../app.js';
+import { MESSAGES } from '../constants.js';
 
 describe('GET /', () => {
-  it('should return Hello, GitHub Actions!', async () => {
+  it('should return the correct greeting message', async () => {
     const res = await request(app).get('/');
     expect(res.status).to.equal(200);
-    expect(res.text).to.equal('Hello, GitHub Actions!');
+    expect(res.text).to.equal(MESSAGES.GREETING);
   });
 });
